019512f3e4986875853802f9ee3d7123bcb73edc
Uzair Aftab <muaf@nmbu.no>
2021-01-16 18:44:25 +0100

    Attempted a fix for issue #1

--- DNB_psd2/PIS/PIS.py
+++ DNB_psd2/PIS/PIS.py
@@ -1,7 +1,12 @@
+import urllib.parse
 from datetime import datetime
 from json import dumps
+from socket import gethostbyname, gethostname
+from urllib.parse import urlparse
+from uuid import uuid4
 
 import requests
+from icecream import ic
 
 from DNB_psd2 import AISP
 
@@ -12,32 +17,32 @@ class PIS:
     docstring
     """
 
-    def __init__(self, pem_path: str, key_path: str, webdriver_path: str) -> None:
+    def __init__(self, pem_path: str, key_path: str) -> None:
+        hostname = gethostname()
+        local_ip = gethostbyname(hostname)
         self.s = requests.Session()
         self.endpoint = "https://sandboxapi.psd.dnb.no/v1/payments"
         headers = {
             "PSU-ID": "TB76688",
-            "PSU-IP-Address": "123.123.123.123",
+            "PSU-IP-Address": local_ip,
             "TPP-Redirect-Preferred": "true",
             "TPP-Redirect-URI": "https://dnb.no",
             "Accept": "application/json",
             "Content-Type": "application/json",
-            "X-Request-ID": "c3bc3320-6ccd-49de-a24d-660f5e99b4e6",
-            "TPP-Explicit-Authorisation-Preferred": "true",
+            "X-Request-ID": str(uuid4()).lower(),
+            # "TPP-Explicit-Authorisation-Preferred": "true",
+            "PSU-User-Agent": "Chrome",
         }
         self.s.cert = (pem_path, key_path)
         self.s.headers.update(headers)
-        self.AISP = AISP(
-            pem_path=pem_path, key_path=key_path, webdriver_path=webdriver_path
-        )
 
     def approval(self):
         response = self.s.request("GET", f"{self.endpoint}/approval", data={})
         return response.text
 
-    def get_domestic_credit_transfers_consent(self):
+    def get_domestic_credit_transfers_consent(self, account: int):
         payload = {
-            "debtorAccount": {"bban": "12035864022"},
+            "debtorAccount": {"bban": f"{account}"},
             "creditorAccount": {"bban": "12094355778"},
             "creditorName": "Foo Bar",
             "instructedAmount": {"amount": 13.37, "currency": "NOK"},
@@ -49,14 +54,53 @@ class PIS:
             f"{self.endpoint}/norwegian-domestic-credit-transfers",
             data=dumps(payload),
         )
-        response = response.json()
-        print(response.get("_links"))
-        return None
+        url = response.json().get("_links").get("scaRedirect").get("href")
+        self.authenticate(url)
+        return response.text
+
+    @staticmethod
+    def authenticate(url):
+        # payload = f"auth_status=Ok&url=%3A%2F%2{url}"
+        ic(url)
+        payload = urllib.parse.quote(url)
+        payload = payload.replace("/", "%2F")
+        payload = f"auth_status=Ok&url={payload}"
+        ic(payload)
+        parsed_url = urlparse(url)
+        headers = {
+            "Accept": "*/*",
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "Origin": f"{parsed_url.scheme}://{parsed_url.netloc}",
+            "Referer": url,
+            "Content-Length": "901",
+            "Host": f"{parsed_url.netloc}",
+            "Accept-Language": "en-gb",
+            "User-Agent": "Chrome",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Connection": "keep-alive",
+            "X-Requested-With": "XMLHttpRequest",
+        }
+        ic(headers)
+        # print(parsed_url)
+        response = requests.request(
+            "POST",
+            f"{parsed_url.scheme}://{parsed_url.netloc}/Prod/bankid/auth",
+            headers=headers,
+            data=payload,
+        )
+        print(response)
 
 
 if __name__ == "__main__":
-    instance = PIS(
-        pem_path="./certificate/certificate.pem", key_path="./certificate/private.key"
+    pem_path = "./certificate/certificate.pem"
+    key_path = "./certificate/private.key"
+    webdriver_path = "./webdriver/chromedriver"
+    AISP_instance = AISP(
+        pem_path=pem_path,
+        key_path=key_path,
+        PSU_ID="TB76688",
     )
-    instance.approval()
-    print(instance.get_domestic_credit_transfers_consent())
+    accounts = AISP_instance.accounts()
+    instance = PIS(pem_path=pem_path, key_path=key_path)
+    a = instance.approval()
+    instance.get_domestic_credit_transfers_consent(account=accounts[0])
